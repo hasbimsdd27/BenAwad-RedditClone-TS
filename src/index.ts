5935da594086cf38451778b6a04b314ba97cebd6
Hasbi Musaddad <musaddadhasbi@gmail.com>
2020-11-06 21:38:05 +0700

    [hasbi] add mikro orm migration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello there")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const post = await orm.em.find(Post, {});
+  console.log(post);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
